29cbd499cbd2667b750cb8d9ce1ec8a66085c37d
GioLorenzo <gio.lorenzo.lui@gmail.com>
2021-01-20 15:29:33 -0500

    more function

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,4 +8,19 @@
   (animateFormIIFE = () => {
     const arrows = document.querySelectorAll('.fa-arrow-down');
     return arrows.forEach(arrow => {
-      arrow.addEv
+      arrow.addEventListener('click', () => validation(arrow));
+      arrow.addEventListener('keypress', (e) => (e.key === 'Enter') ? validation(arrow) : null);
+    });
+  })();
+
+  function validation(arrow) {
+    const input = arrow.previousElementSibling;
+    const parent = arrow.parentElement;
+    const nextForm = parent.nextElementSibling;
+
+    // Check for validation
+    if (input.type === 'text' && isSixCharacters(input)) {
+      nextSlide(parent, nextForm);
+    } else if (input.type === 'email' && validateEmail(input)) {
+      nextSlide(parent, nextForm);
+    
